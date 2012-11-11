2111e31ac8e8d03c615508d8412b5756db01744f
Zachary Kain <zakkain@gmail.com>
2012-11-10 19:10:22 -0500

    helpful comments

--- app/scripts/agora.js
+++ app/scripts/agora.js
@@ -3,6 +3,8 @@
 // Declare app level module which depends on filters, and services
 var agoraApp = angular.module('agoraApp', [])
   .config(['$routeProvider', function($routeProvider) {
+
+    // here put conditional cookie logic for logged in or out
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
